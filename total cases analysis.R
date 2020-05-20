8c2e0899b4d88bdd532e16ff5e4d4567c23b6856
EduardoCosta <j.eduardovilela@gmail.com>
2020-05-20 09:14:51 -0300

    retirada barras de espaço dos nomes dos arquivos

--- total cases analysis.R
+++ total cases analysis.R
@@ -10,7 +10,7 @@ for(var in unique(df_estados$uf))
   p1 <- ggplot(data=df_estados[df_estados$uf == var,]) + 
               geom_line(stat= "identity", 
                        aes(x=datetime, y=cases), size=2, colour="blue")+
-              labs(title = paste("Casos totais no estado de",var, "por dia" ),
+              labs(title = paste("Casos totais no estado de",var,"por dia"),
                     subtitle = "",
                     caption = paste("Origem dos dados:",origem,"\n Atualizado em:", last_update,"   -  Autor: Eduardo Costa - duducosta.github.io/Covid19-Brazil/"))+
               xlab("Days") + ylab("Casos totais") +
@@ -19,7 +19,7 @@ for(var in unique(df_estados$uf))
                       axis.text = element_text(face="bold",size=15),
                     plot.title = element_text(lineheight = 0.8,face="bold",size=30),
                     plot.caption = element_text(lineheight = 0.8,size=15))
-  jpeg(paste(var,"-TC-Basico.jpeg"), width= g_width, height= g_height)
+  jpeg(paste(var,"-TC-Basico.jpeg",sep=""), width= g_width, height= g_height)
   print(p1)
   dev.off() 
 }
@@ -45,7 +45,7 @@ for(var in unique(df_estados$uf))
           axis.text = element_text(face="bold",size=15),
           plot.title = element_text(lineheight = 0.8,face="bold",size=30),
           plot.caption = element_text(lineheight = 0.8,size=15))
-  jpeg(paste(var,"-TC-Filtro.jpeg"), width= g_width, height= g_height)
+  jpeg(paste(var,"-TC-Filtro.jpeg",sep=""), width= g_width, height= g_height)
   print(p3)
   dev.off()
   remove(filteredVector)
@@ -75,7 +75,7 @@ for(var in unique(df_estados$uf))
   d <- df_temp
   tt <- ttheme_default(colhead=list(fg_params = list(parse=TRUE)))
   colnames(d) <- c("Estado", "Semana epidem.", "Casos totais", "Aumento de casos da semana", "Média de aumento por dia (/1000)", "Velocidade do aumento")
-  jpeg((paste(var,"-Vel_semanal.jpeg")), width= g_width, height= g_height)
+  jpeg((paste(var,"-Vel_semanal.jpeg",sep="")), width= g_width, height= g_height)
   grid.newpage()
   grid.table(d)
   dev.off()  
@@ -104,7 +104,7 @@ for(var in unique(df_estados$uf))
           legend.text = element_text(size=15,face="bold"),
           legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"),
           legend.position = "bottom")
-  jpeg(paste(var,"-TC-Completo.jpeg"), width= g_width, height= g_height)
+  jpeg(paste(var,"-TC-Completo.jpeg",sep=""), width= g_width, height= g_height)
   print(p1)
   dev.off()
   df_estados$TanAngle <-NULL
@@ -127,7 +127,7 @@ p1 <- ggplot(data=p1_data) +
         axis.text = element_text(face="bold",size=15),
         plot.title = element_text(lineheight = 0.8,face="bold",size=30),
         plot.caption = element_text(lineheight = 0.8,size=15))
-jpeg("0 Brasil-TC-Basico.jpeg") width= g_width, height= g_height)
+jpeg("0Brasil-TC-Basico.jpeg", width= g_width, height= g_height)
 print(p1)
 dev.off() 
 remove(p1)
@@ -150,7 +150,7 @@ p3 <- ggplot(data=p3_data) +
         axis.text = element_text(face="bold",size=15),
         plot.title = element_text(lineheight = 0.8,face="bold",size=30),
         plot.caption = element_text(lineheight = 0.8,size=15))
-jpeg("0 Brasil-TC-ComFiltro.jpeg", width= g_width, height= g_height)
+jpeg("0Brasil-TC-ComFiltro.jpeg", width= g_width, height= g_height)
 print(p3)
 dev.off() 
 remove(p3)
@@ -166,7 +166,7 @@ remove(var,po, no, p1_data, filteredVector)
 
 
 # 2.3 Treemap com última posição de casos totais
-jpeg("0 Brasil-TC-Treemap.jpeg", width=1000, height=800)
+jpeg("0Brasil-TC-Treemap.jpeg", width=1000, height=800)
 p2_data <- df_estados[df_estados$datetime==max(df_estados$datetime),]
 p2_data$percentage <- round(p2_data$cases/sum(p2_data$cases)*100,1)
 p2 <- treemap(dtf = p2_data, 
@@ -193,15 +193,15 @@ remove(p2)
 
 tt <- ttheme_default(colhead=list(fg_params = list(parse=TRUE)))
 colnames(p2_data) <- c("Estado", "Casos totais", "Percentual")
-jpeg("0 Brasil-TC-Treemap-Tab1.jpeg", width= g_width, height= g_height)
+jpeg("0Brasil-TC-Treemap-Tab1.jpeg", width= g_width, height= g_height)
 grid.newpage()
 grid.table(p2_data[1:10,])
 dev.off() 
-jpeg("0 Brasil-TC-Treemap-Tab2.jpeg", width= g_width, height= g_height)
+jpeg("0Brasil-TC-Treemap-Tab2.jpeg", width= g_width, height= g_height)
 grid.newpage()
 grid.table(p2_data[11:20,])
 dev.off() 
-jpeg("0 Brasil-TC-Treemap-Tab3.jpeg", width= g_width, height= g_height)
+jpeg("0Brasil-TC-Treemap-Tab3.jpeg", width= g_width, height= g_height)
 grid.newpage()
 grid.table(p2_data[21:27,])
 dev.off() 
